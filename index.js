2704e550575eb1d00d664bb5d0c7dab02d0fb935
Lucianoaj <lucianoajadv@gmail.com>
2023-12-15 17:20:56 -0300

    explicando como atualizar

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("primeiro desafio dio.me")
 console.log("teste 2")
+console.log("para atualizar o code no githun, basta rodar o code, depois comando git add . seguido de git commit -m recado seguido de git push -u origin main agora f5 no github projetos")
