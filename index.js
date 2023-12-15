f6a697e6602e83c9fdc0dc166b6332fda90c22fd
Lucianoaj <lucianoajadv@gmail.com>
2023-12-15 17:46:31 -0300

    desafio do herói

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ console.log("teste 2")
 console.log("para atualizar o code no githun, basta rodar o code, depois comando git add . seguido de git commit -m recado seguido de git push -u origin main agora f5 no github projetos")
 console.log("uma utilidade legal é que o github se torna sua nuvem de projetos podendo abrir em qualquer maquina, basta clonar no vscode")
 console.log("em terminal de um git clone e o link do git")
+//primeiro Desafio Classificador de nível de Herói 
+// O que deve ser utilizado:
+// - variáveis - operadores - laços de repetição - estruturas de decisões
+// objetivo: criar uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar algumas das mensagens abaixo:
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 6.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000 = Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+// Final(saida): ao final deve se exibir uma mensagem: "O Herói de nome**{name}** está no nível de **{nivel xp}"
