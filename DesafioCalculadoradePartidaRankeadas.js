7f07949f566e02359cacbefa1e94a649459a127a
Luciano AJJ <153945280+Lucianoaj@users.noreply.github.com>
2023-12-20 11:08:09 -0300

    DesafioCalculadoradePartidaRankeadas.js

--- DesafioCalculadoradePartidaRankeadas.js
+++ DesafioCalculadoradePartidaRankeadas.js
@@ -1,25 +1,10 @@
-// Calculadora de partidas Rankeadas
-// o que deve ser utilizado: - variaveis, -operadores, -laçoes de repetição, - estruturas de decisões, - funções.
-// Objetivo: CRIE UMA FUNÇÃO QUE RECEBE COMO PARÂMETRO A QUANTIDADE DE VITÓRIAS E DERROTAS DE UM JOGADOR, DEPOIS DISSO RETORNE O RESULTADO PARA UMA VARIÁVEL, O SALDO DE RANKEADOS
-//DEVE SER FEITO ATRAVÉS DO CALCULO (VITÓRIAS - DERROTAS)
-// SE vitórias for menor do que 10 = FERRO
-// SE vitórias for entre 11 e 20 = BRONZE
-// SE vitórias for entre 21 e 50 = prata
-// SE vitórias for entre 51 e 80 = ouro
-// SE vitórias for entre 81 e 90 =  DIAMENTE
-// SE vitórias for entre 91 e 100 = LENDÁRIO
-// SE vitórias for maior ou igual a 101 = IMORTAL
-// # saída: Ao final deve se exibir uma mensagem: "O Herói tem de saldo de **(saldoVitórias)** está no nível de **(nível)**
-
 let balance = difference(39, 17)
 function difference(winner, loser){
    let differenceBalance = winner - loser 
    return differenceBalance
 }
-
 let nivel = ["FERRO", "BRONZE", "PRATA", "OURO", "DIAMANTE", "LENDÁRIO", "IMORTAL"]
 let iN = 0
-
 if(balance <=10){
     console.log("O Herói tem saldo de " + balance + " está no nível de " + nivel[iN])
 }
